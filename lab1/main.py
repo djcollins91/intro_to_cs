1da8f69249ed584d74acde37e06f372d6214ad95
Dan Collins <cdan200@icloud.com>
2025-06-11 22:54:00 -0600

    Lab1 Complete

--- lab1/main.py
+++ lab1/main.py
@@ -1,45 +1,25 @@
 def add(a, b):
     """Returns the sum of a and b."""
-    pass
+    return a + b
 def subtract(a, b):
     """Returns the difference of a and b."""
-    pass
+    return a - b
 def multiply(a, b):
     """Returns the product of a and b."""
-    pass
-def divide(a, b):
-    """Returns the quotient of a and b."""
-    pass
+    return a * b
+
 def modulus(a, b):
     """Returns the modulus of a and b."""
-    pass
+    return a % b
 def exponentiate(base, exp):
     """Returns base raised to the power of exp."""
-    pass
-def floor_divide(a, b):
-    """Returns the floor division of a by b."""
-    pass
+    return base ** exp
+
 def absolute_value(x):
     """Returns the absolute value of x."""
-    pass
-def square_root(x):
-    """Returns the square root of x."""
-    pass
+    return abs(x)
+
+    
 def power(base, exp):
     """Returns base raised to the power of exp."""
-    pass
-def logarithm(value, base):
-    """Returns the logarithm of value to the given base."""
-    pass
-def factorial(n):
-    """Returns the factorial of n."""
-    pass
-def gcd(a, b):
-    """Returns the greatest common divisor of a and b."""
-    pass
-def lcm(a, b):
-    """Returns the least common multiple of a and b."""
-    pass
-def is_prime(n):
-    """Returns True if n is a prime number, otherwise False."""
-    pass
+    return base ** exp
